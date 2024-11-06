72cba645a18ae2b64a9cf06e4ce9ee8330b8d1d1
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-06 14:32:37 +0200

    upd: user errors

--- src/ImpoBooks/ImpoBooks.BusinessLogic/Errors/ErrorTypes.cs
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Errors/ErrorTypes.cs
@@ -4,4 +4,6 @@ public static class ErrorTypes
 {
     public const int AlreadyExists = 1;
     public const int IsNull = 2;
+    public const int NotFound = 3;
+    public const int WrongInfo = 4;
 }
--- src/ImpoBooks/ImpoBooks.BusinessLogic/Errors/Users/UserErrors.cs
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Errors/Users/UserErrors.cs
@@ -8,5 +8,11 @@ public static class UserErrors
         Error.Custom(ErrorTypes.AlreadyExists, "User.Exists", "User with this email already exists.");
     
     public static Error IsNull =>
-        Error.Custom(ErrorTypes.IsNull, "User.IsNull", "User was null.");
+        Error.Custom(ErrorTypes.IsNull, "User.IsNull", "User was null.");    
+    
+    public static Error NotFoundByEmail =>
+        Error.Custom(ErrorTypes.NotFound, "User.NotFoundByEmail", "User with this email does not exist.");
+  
+    public static Error WrongPassword =>
+        Error.Custom(ErrorTypes.WrongInfo, "User.WrongPassword", "Password was invalid.");
 }
