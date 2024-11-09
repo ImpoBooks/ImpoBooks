970e448f1968b5aba076449227d85d49cf9ce8c0
Nazg0r <gorchinshiy83@gmail.com>
2024-11-09 13:55:50 +0200

    feat: implement Person repository

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Interfaces/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Interfaces
+{
+	internal interface IPersonRepository : IRepository<Person>
+	{
+		Task<IEnumerable<Person>> GetByFullNameAsync(string name, string surname);
+	}
+}
---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Repositories/PersonRepository.cs
@@ -0,0 +1,24 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using ImpoBooks.DataAccess.Interfaces;
+using Supabase;
+using Supabase.Postgrest.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Repositories
+{
+	public class PersonRepository(Client client) : Repository<Person>(client), IPersonRepository
+	{
+		private readonly Client _client = client;
+
+		public async Task<IEnumerable<Person>> GetByFullNameAsync(string name, string surname)
+		{
+			ModeledResponse<Person> response = await _client.From<Person>().Where(x => x.Name == name && x.Surnmae == surname).Get();
+			IEnumerable<Person> person = response.Models;
+			return person;
+		}
+	}
+}
