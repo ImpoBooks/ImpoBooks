b3a23e41ae045050b4fcd29937f965409bd8e3d1
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-09 22:19:14 +0200

    upd: moving errors to infrastructure

--- src/ImpoBooks/ImpoBooks.Infrastructure/Errors/ErrorTypes.cs
+++ src/ImpoBooks/ImpoBooks.Infrastructure/Errors/ErrorTypes.cs
@@ -6,4 +6,5 @@ public static class ErrorTypes
     public const int IsNull = 2;
     public const int NotFound = 3;
     public const int WrongInfo = 4;
+    public const int NullProperties = 5;
 }
--- src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Users/UserErrors.cs
+++ src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Users/UserErrors.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using ErrorOr;
 
 namespace ImpoBooks.BusinessLogic.Errors.Users;
@@ -6,13 +7,24 @@ public static class UserErrors
 {
     public static Error AlreadyExists =>
         Error.Custom(ErrorTypes.AlreadyExists, "User.Exists", "User with this email already exists.");
-    
+
     public static Error IsNull =>
-        Error.Custom(ErrorTypes.IsNull, "User.IsNull", "User was null.");    
-    
+        Error.Custom(ErrorTypes.IsNull, "User.IsNull", "User was null.");
+
     public static Error NotFoundByEmail =>
         Error.Custom(ErrorTypes.NotFound, "User.NotFoundByEmail", "User with this email does not exist.");
-  
+
     public static Error WrongPassword =>
         Error.Custom(ErrorTypes.WrongInfo, "User.WrongPassword", "Password was invalid.");
+
+    public static Error NullProperties(List<string> propertiesNames)
+    {
+        StringBuilder errorDesription = new StringBuilder();
+        errorDesription.Append("Properties can not be null or empty:");
+
+        foreach (var propertyName in propertiesNames)
+            errorDesription.Append($" {propertyName},");
+
+        return Error.Custom(ErrorTypes.NullProperties, "User.NullProperties", errorDesription.ToString().TrimEnd(','));
+    }
 }
