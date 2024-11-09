c77813ff915d10f8552d365789075a14c517d639
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-09 22:18:21 +0200

    upd: user profile management

---
+++ src/ImpoBooks/ImpoBooks.Server/Responses/UserProfileResponse.cs
@@ -0,0 +1,8 @@
+namespace ImpoBooks.Server.Responses;
+
+public class UserProfileResponse
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
