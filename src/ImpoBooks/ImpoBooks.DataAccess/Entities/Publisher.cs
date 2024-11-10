f27a0186c3365110eb0ad2688a781d3a25ed37b8
Nazg0r <gorchinshiy83@gmail.com>
2024-11-10 14:47:04 +0200

    feat: create Publisher entity

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Entities/Publisher.cs
@@ -0,0 +1,29 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Entities
+{
+	[Table("Publishers")]
+	public class Publisher : BaseModelExtended
+	{
+		[Column("name")] public string Name { get; set; }
+		//[Reference(typeof(Book))] public ICollection<Book> Books { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			return obj is Publisher publisher &&
+			Id == publisher.Id &&
+			Name == publisher.Name;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Id);
+		}
+	}
+}
