336cdab7598a680880733a7e53601f2cc1c5072f
Nazg0r <gorchinshiy83@gmail.com>
2024-11-10 16:02:36 +0200

    feat: implement Genre repository

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Interfaces/IGenreRepository.cs
@@ -0,0 +1,14 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Interfaces
+{
+	internal interface IGenreRepository : IRepository<Genre>
+	{
+		Task<Genre> GetByNameAsync(string name);
+	}
+}
---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Repositories/GenreRepository.cs
@@ -0,0 +1,24 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using ImpoBooks.DataAccess.Interfaces;
+using Supabase;
+using Supabase.Postgrest.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Repositories
+{
+	public class GenreRepository(Client client) : Repository<Genre>(client), IGenreRepository
+	{
+		private readonly Client _client = client;
+
+		public async Task<Genre> GetByNameAsync(string name)
+		{
+			ModeledResponse<Genre> response = await _client.From<Genre>().Where(x => x.Name == name).Get();
+			Genre genre = response.Model;
+			return genre;
+		}
+	}
+}
