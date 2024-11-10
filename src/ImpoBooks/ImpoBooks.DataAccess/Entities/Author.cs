f35625cabacc47cd959ea990673cd88382272bd6
Nazg0r <gorchinshiy83@gmail.com>
2024-11-10 14:25:25 +0200

    feat: create Author entity

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Entities/Author.cs
@@ -0,0 +1,35 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ImpoBooks.DataAccess.Entities
+{
+	[Table("Authors")]
+	public class Author : BaseModelExtended
+	{
+		[Column("person_id")] public int PersonId { get; set; }
+		[Reference(typeof(Person))] public Person Person { get; set; }
+
+		//[Reference(typeof(Book))] public ICollection<Book> Books { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			return obj is Author author &&
+			Id == author.Id &&
+			PersonId == author.PersonId &&
+			Person.Id == author.Person.Id &&
+			Person.Name == author.Person.Name &&
+			Person.Surname == author.Person.Surname;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Id);
+		}
+	}
+}
