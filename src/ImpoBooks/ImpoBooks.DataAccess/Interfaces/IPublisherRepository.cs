6ba657567859e88e677bf6a60620ebe377621db2
Nazg0r <gorchinshiy83@gmail.com>
2024-11-10 15:31:04 +0200

    feat: implement Publisher repository

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Interfaces/IPublisherRepository.cs
@@ -0,0 +1,14 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Interfaces
+{
+	internal interface IPublisherRepository : IRepository<Publisher>
+	{
+		Task<Publisher> GetByNameAsync(string name);
+	}
+}
---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Repositories/PublisherRepository.cs
@@ -0,0 +1,24 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using ImpoBooks.DataAccess.Interfaces;
+using Supabase;
+using Supabase.Postgrest.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Repositories
+{
+	public class PublisherRepository(Client client) : Repository<Publisher>(client), IPublisherRepository
+	{
+		private readonly Client _client = client;
+
+		public async Task<Publisher> GetByNameAsync(string name)
+		{
+			ModeledResponse<Publisher> response = await _client.From<Publisher>().Where(x => x.Name == name).Get();
+			Publisher publisher = response.Model;
+			return publisher;
+		}
+	}
+}
