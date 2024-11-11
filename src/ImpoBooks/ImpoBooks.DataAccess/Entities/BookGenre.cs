f28f526d58c5e82b04be5a98b1805a583a55033f
Nazg0r <gorchinshiy83@gmail.com>
2024-11-11 20:37:32 +0200

    feat: create Book-Genre relation entity

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Entities/BookGenre.cs
@@ -0,0 +1,20 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Entities
+{
+	[Table("BooksGenres")]
+	public class BookGenre : BaseModelExtended
+	{
+		[Column("book_id")] public int BookId { get; set; }
+		[Column("genre_id")] public int GenreId { get; set; }
+
+		[Reference(typeof(Book))] public Book Book { get; set; }
+		[Reference(typeof(Genre))] public Genre Genre { get; set; }
+	}
+}
