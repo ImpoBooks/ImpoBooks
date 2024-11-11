344b2d0769cd3216b98a564c6245a3588c0c131b
Nazg0r <gorchinshiy83@gmail.com>
2024-11-11 20:38:10 +0200

    feat: create Book entity

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Entities/Book.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Entities
+{
+	[Table("Books")]
+	public class Book : BaseModelExtended
+	{
+		[Column("name")] public string Name { get; set; }
+		[Column("publisher_id")] public int PublisherId { get; set; }
+		[Column("author_id")] public int AuthorId { get; set; }
+		[Column("description")] public string Description { get; set; }
+		[Column("release_date")] public DateTime ReleaseDate { get; set; }
+		[Column("price")] public decimal Price { get; set; }
+		[Column("rating")] public decimal Rating { get; set; }
+		[Column("format")] public string Format { get; set; }
+		[Reference(typeof(Publisher))] public Publisher Publisher { get; set; }
+		[Reference(typeof(Author))] public Author Author { get; set; }
+		[Column("genres")] public ICollection<Genre>? Genres { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			return obj is Book book &&
+			Id == book.Id &&
+			Name == book.Name &&
+			PublisherId == book.PublisherId &&
+			AuthorId == book.AuthorId &&
+			Description == book.Description &&
+			ReleaseDate.Equals(book.ReleaseDate.AddDays(1)) &&
+			Price == book.Price &&
+			Rating == book.Rating &&
+			Format == book.Format &&
+			Publisher.Equals(book.Publisher) &&
+			Author.Equals(book.Author) &&
+			Genres?.Count == book.Genres?.Count;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Id);
+		}
+	}
+}
