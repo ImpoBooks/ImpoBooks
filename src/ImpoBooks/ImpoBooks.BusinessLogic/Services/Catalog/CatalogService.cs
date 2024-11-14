9d58b79689637f0d214e5141f87f8b761406f2d3
Nazg0r <gorchinshiy83@gmail.com>
2024-11-14 18:00:14 +0200

    feat: implement get books service

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Catalog/CatalogService.cs
@@ -0,0 +1,47 @@
+﻿using ErrorOr;
+using ImpoBooks.BusinessLogic.Services.Extensions;
+using ImpoBooks.BusinessLogic.Services.Mapping;
+using ImpoBooks.BusinessLogic.Services.Models;
+using ImpoBooks.DataAccess.Entities;
+using ImpoBooks.DataAccess.Interfaces;
+using ImpoBooks.DataAccess.Repositories;
+using ImpoBooks.Infrastructure.Errors.Catalog;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Catalog
+{
+	public class CatalogService(IBookRepository bookRepository) : ICatalogService
+	{
+		private readonly IBookRepository _bookRepository = bookRepository;
+		public async Task<ErrorOr<IEnumerable<CatalogBookModel>>> GetBooksAsync(FilterModel filterOptions)
+		{
+			if (filterOptions is null)
+				return CatalogErrors.IsNull;
+
+			IEnumerable<Book> dbBooks = await _bookRepository.GetAllAsync();
+
+			if (dbBooks is null)
+				return CatalogErrors.BooksNotFound;
+
+			IEnumerable<Book> filteredBooks = dbBooks.FilterByKeyWord(filterOptions.KeyWords)
+				.FilterByGenre(filterOptions.Genre)
+				.FilterByAuthor(filterOptions.Author)
+				.FilterByPrice(filterOptions.MinPrice, filterOptions.MaxPrice)
+				.FilterByRating(filterOptions.MinRating, filterOptions.MaxRating);
+
+			if (filteredBooks is null)
+				return CatalogErrors.NoBookMatches;
+
+			IEnumerable<CatalogBookModel> result = filteredBooks.Select(b => b.ToCatalogBookModel());
+
+			return result.ToErrorOr();
+		}
+
+		
+	}
+}
---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Catalog/ICatalogService.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using ImpoBooks.BusinessLogic.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Catalog
+{
+	public interface ICatalogService
+	{
+		Task<ErrorOr<IEnumerable<CatalogBookModel>>> GetBooksAsync(FilterModel filterOptions);
+	}
+}
