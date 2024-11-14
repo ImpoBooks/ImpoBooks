ae9041e5ff30cbb361858e87bce7afdf40802274
Nazg0r <gorchinshiy83@gmail.com>
2024-11-14 17:50:26 +0200

    feat: add mapper to book model

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Mapping/EntityToModel.cs
@@ -0,0 +1,34 @@
+﻿using ImpoBooks.BusinessLogic.Services.Models;
+using ImpoBooks.DataAccess.Entities;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Mapping
+{
+	public static class EntityToModel
+	{
+		public static CatalogBookModel ToCatalogBookModel(this Book source)
+		{
+			StringBuilder sb = new StringBuilder();
+			string authorFullName = sb.Append(source.Author.Person.Name)
+				.Append(" ")
+				.Append(source.Author.Person.Surname)
+				.ToString();
+
+			return new CatalogBookModel() 
+			{
+				Id = source.Id,
+				Name = source.Name,
+				Author = authorFullName,
+				Genres = string.Join(", ", source.Genres!),
+				ReleaseDate = source.ReleaseDate,
+				Rating = source.Rating,
+				Price = source.Price,
+			};
+		}
+	}
+}
