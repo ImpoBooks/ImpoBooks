682ff837e1e95bd0805bb53b025c77c07a2bd3b3
Nazg0r <gorchinshiy83@gmail.com>
2024-11-14 17:48:13 +0200

    feat: add book and filter models

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Models/FilterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Models
+{
+	public class FilterModel
+	{
+		public string? KeyWords { get; set; }
+		public string? Genre { get; set; }
+		public string? Author { get; set; }
+		public decimal MinPrice { get; set; }
+		public decimal MaxPrice { get; set; }
+		public decimal MinRating { get; set; }
+		public decimal MaxRating { get; set; }
+	}
+}
