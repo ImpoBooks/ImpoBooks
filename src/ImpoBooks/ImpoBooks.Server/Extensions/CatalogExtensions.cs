013a6bf306a69b4dc250a7cc1f439a707e313d9c
Nazg0r <gorchinshiy83@gmail.com>
2024-11-14 18:02:44 +0200

    feat: add catalog request convertor

---
+++ src/ImpoBooks/ImpoBooks.Server/Extensions/CatalogExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ImpoBooks.BusinessLogic.Services.Models;
+using ImpoBooks.Server.Requests;
+
+namespace ImpoBooks.Server.Extensions
+{
+	public static class CatalogExtensions
+	{
+		public static FilterModel ToModel(this CatalogRequest source)
+		{
+			return new FilterModel() 
+			{ 
+				KeyWords = source.KeyWords,
+				Genre = source.Genre,
+				Author	= source.Author,
+				MinPrice = source.MinPrice,
+				MaxPrice = source.MaxPrice,
+				MinRating = source.MinRating,
+				MaxRating = source.MaxRating,
+			};
+		}
+	}
+}
