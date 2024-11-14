491e8b0f434dc15570e4342847a07f3cbc097612
Nazg0r <gorchinshiy83@gmail.com>
2024-11-14 18:03:46 +0200

    feat: add Catalog controller

---
+++ src/ImpoBooks/ImpoBooks.Server/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using ErrorOr;
+using ImpoBooks.BusinessLogic.Services.Catalog;
+using ImpoBooks.BusinessLogic.Services.Models;
+using ImpoBooks.Server.Extensions;
+using ImpoBooks.Server.Requests;
+using ImpoBooks.Server.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImpoBooks.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CatalogController(ICatalogService catalogService) : Controller
+	{
+		private readonly ICatalogService _catalogService = catalogService;
+
+		[HttpGet]
+		[ProducesResponseType<IEnumerable<CatalogBookModel>>(StatusCodes.Status200OK)]
+		[ProducesResponseType<List<Error>>(StatusCodes.Status400BadRequest)]
+		public async Task<IResult> GetBooks(CatalogRequest catalogRequest)
+		{
+			ErrorOr<IEnumerable<CatalogBookModel>> result =
+				await _catalogService.GetBooksAsync(catalogRequest.ToModel());
+
+			return result.Match(
+				books => Results.Ok(books),
+				errors => Results.BadRequest(errors.First())
+			);
+		}
+	}
+}
