5a831d11f507addc3da5a93b8f8167b8d0b38153
Nazg0r <gorchinshiy83@gmail.com>
2024-11-15 18:08:25 +0200

    feat: add Book model

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Models/BookModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Models
+{
+	public class BookModel
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Author { get; set; }
+		public string Genres { get; set; }
+		public string Publisher { get; set; }
+		public string ReleaseDate { get; set; }
+		public string Image { get; set; }
+		public decimal Rating { get; set; }
+		public decimal Price { get; set; }
+	}
+}
