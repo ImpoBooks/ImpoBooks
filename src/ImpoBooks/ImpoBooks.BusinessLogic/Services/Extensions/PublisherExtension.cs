ebeba0b7488d41bc93441405fe1ca03dfdd55d2e
Nazg0r <gorchinshiy83@gmail.com>
2024-11-15 18:11:09 +0200

    feat: add check method to publisher entity

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Extensions/PublisherExtension.cs
@@ -0,0 +1,29 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using ImpoBooks.DataAccess.Interfaces;
+using ImpoBooks.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Extensions
+{
+	public static class PublisherExtension
+	{
+		public async static Task<Publisher> CreateIfNotExistAsync(this Publisher source, IPublisherRepository publisherRepository, string name)
+		{
+			if (source is null)
+			{
+				await publisherRepository.CreateAsync(new Publisher()
+				{
+					Name = name
+				});
+
+				return await publisherRepository.GetByNameAsync(name);
+			}
+			
+			return source;
+		}
+	}
+}
