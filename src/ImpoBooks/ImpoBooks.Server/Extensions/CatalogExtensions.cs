c83ee5069cb8c742d1a2644d400fa7d1b2853c27
Nazg0r <gorchinshiy83@gmail.com>
2024-11-15 18:05:58 +0200

    feat: add ToModel methods to new request models

--- src/ImpoBooks/ImpoBooks.Server/Extensions/CatalogExtensions.cs
+++ src/ImpoBooks/ImpoBooks.Server/Extensions/CatalogExtensions.cs
@@ -18,5 +18,37 @@ namespace ImpoBooks.Server.Extensions
 				MaxRating = source.MaxRating,
 			};
 		}
+
+		public static BookModel ToModel(this CatalogCreateRequest source)
+		{
+			return new BookModel()
+			{
+				Name = source.Name,
+				Description = source.Description,
+				Author = source.Author,
+				Genres = source.Genre,
+				Publisher = source.Publisher,
+				ReleaseDate = source.ReleaseDate,
+				Image = source.Image,
+				Rating = 0,
+				Price = source.Price,
+			};
+		}
+
+		public static BookModel ToModel(this CatalogUpdateRequest source)
+		{
+			return new BookModel()
+			{
+				Name = source.Name,
+				Description = source.Description,
+				Author = source.Author,
+				Genres = source.Genre,
+				Publisher = source.Publisher,
+				ReleaseDate = source.ReleaseDate,
+				Image = source.Image,
+				Rating = 0,
+				Price = source.Price,
+			};
+		}
 	}
 }
