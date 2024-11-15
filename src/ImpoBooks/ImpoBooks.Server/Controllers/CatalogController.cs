e89f9237a8abdb50a1f8a260b87516d3c31a9e36
Nazg0r <gorchinshiy83@gmail.com>
2024-11-15 18:15:01 +0200

    feat: add CreateBook controller

--- src/ImpoBooks/ImpoBooks.Server/Controllers/CatalogController.cs
+++ src/ImpoBooks/ImpoBooks.Server/Controllers/CatalogController.cs
@@ -27,5 +27,19 @@ namespace ImpoBooks.Server.Controllers
 				errors => Results.BadRequest(errors.First())
 			);
 		}
+
+		[HttpPost("admin/books/create")]
+		[ProducesResponseType<CatalogBookModel>(StatusCodes.Status201Created)]
+		[ProducesResponseType<List<Error>>(StatusCodes.Status400BadRequest)]
+		public async Task<IResult> CreateBook(CatalogCreateRequest catalogRequest)
+		{
+			ErrorOr<CatalogBookModel> result =
+				await _catalogService.CreateBookAsync(catalogRequest.ToModel());
+
+			return result.Match(
+				books => Results.Ok(books),
+				errors => Results.BadRequest(errors.First())
+			);
+		}
 	}
 }
