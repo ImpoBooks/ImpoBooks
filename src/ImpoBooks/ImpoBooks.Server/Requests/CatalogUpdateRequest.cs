d621f4b7aba7b43ec240ec9873768b1f802f8c3a
Nazg0r <gorchinshiy83@gmail.com>
2024-11-15 18:02:17 +0200

    feat: add new request models

---
+++ src/ImpoBooks/ImpoBooks.Server/Requests/CatalogUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace ImpoBooks.Server.Requests
+{
+	public class CatalogUpdateRequest
+	{
+		public string? Name { get; set; }
+		public string? Genre { get; set; }
+		public string? Author { get; set; }
+		public string? Publisher { get; set; }
+		public string? ReleaseDate { get; set; }
+		public string? Description { get; set; }
+		public string? Image { get; set; }
+		public decimal Price { get; set; }
+	}
+}
