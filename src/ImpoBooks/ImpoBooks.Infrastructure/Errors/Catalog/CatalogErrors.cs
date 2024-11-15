3a550ce6c4d7fccfb717028866017358b4eb92c9
Nazg0r <gorchinshiy83@gmail.com>
2024-11-15 18:07:13 +0200

    upd: add new Error types to catalog

--- src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Catalog/CatalogErrors.cs
+++ src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Catalog/CatalogErrors.cs
@@ -12,13 +12,21 @@ namespace ImpoBooks.Infrastructure.Errors.Catalog
 {
 	public static class CatalogErrors
 	{
-		public static Error IsNull =>
+		public static Error FilterIsNull =>
 			Error.Custom(ErrorTypes.IsNull, "FilterOptions.IsNull", "Filter object was null.");
+		public static Error BookIsNull =>
+			Error.Custom(ErrorTypes.IsNull, "Book.IsNull", "Book object was null.");
 
 		public static Error BooksNotFound =>
 			Error.Custom(ErrorTypes.NotFound, "Books.NotFound", "Failed to get books.");
 
+		public static Error BookNotFound =>
+			Error.Custom(ErrorTypes.NotFound, "Book.NotFound", "Failed to get book.");
+
 		public static Error NoBookMatches =>
 			Error.Custom(ErrorTypes.NotFound, "Books.NoBookMatches", "No books were found for the established criterias");
+
+		public static Error AlreadyExists =>
+			Error.Custom(ErrorTypes.NotFound, "Book.AlreadyExists", "Book with this name already exists.");
 	}
 }
