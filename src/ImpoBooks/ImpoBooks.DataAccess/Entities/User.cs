c90d2dd27624fd00bc78cf9dab77897b7b0f32d0
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-18 23:05:53 +0200

    upd: adding role fields to user

--- src/ImpoBooks/ImpoBooks.DataAccess/Entities/User.cs
+++ src/ImpoBooks/ImpoBooks.DataAccess/Entities/User.cs
@@ -1,16 +1,18 @@
 using ImpoBooks.DataAccess.Entities.AutoIncremented;
 using ImpoBooks.DataAccess.Interfaces;
 using Supabase.Postgrest.Attributes;
-using Supabase.Postgrest.Models;
 
 namespace ImpoBooks.DataAccess.Entities;
 
 [Table("Users")]
 public class User : BaseModelExtended, IAutoInc<AutoIncUser>
 {
+    [PrimaryKey("id", false)] public int Id { get; set; }
     [Column("name")] public string Name { get; set; }
     [Column("email")] public string Email { get; set; }
     [Column("hashed_password")] public string HashedPassword { get; set; }
+    [Column("role_id")] public int RoleId { get; set; }
+    [Reference(typeof(Role))] public Role Role { get; set; }
 
 	public override bool Equals(object obj)
 	{
