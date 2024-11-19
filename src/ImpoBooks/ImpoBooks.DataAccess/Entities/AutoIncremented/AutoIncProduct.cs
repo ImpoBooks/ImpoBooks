f2841cba182cfa736f8555c247af597e01c52e96
Nazg0r <gorchinshiy83@gmail.com>
2024-11-19 22:30:32 +0200

    feat: add product entity

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Entities/AutoIncremented/AutoIncProduct.cs
@@ -0,0 +1,16 @@
+﻿using Supabase.Postgrest.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Entities.AutoIncremented
+{
+	public class AutoIncProduct : BaseModelAutoIncExtended
+	{
+		[Column("book_id")] public int BookId { get; set; }
+		[Reference(typeof(Book))] public Book Book { get; set; }
+		[Column("coments")] public ICollection<Comment> Comments { get; set; }
+	}
+}
---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Entities/Product.cs
@@ -0,0 +1,47 @@
+﻿using ImpoBooks.DataAccess.Entities.AutoIncremented;
+using Microsoft.IdentityModel.Tokens;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Xml.Linq;
+using ImpoBooks.DataAccess.Interfaces;
+
+namespace ImpoBooks.DataAccess.Entities
+{
+	[Table("Products")]
+	public class Product : BaseModelExtended, IAutoInc<AutoIncProduct>
+	{
+		[Column("book_id")] public int BookId { get; set; }
+		[Reference(typeof(Book))] public Book Book { get; set; }
+		[Column("comments")] public ICollection<Comment> Comments { get; set; }
+		public override bool Equals(object obj)
+		{
+			return obj is Product product &&
+			Id == product.Id &&
+			BookId == product.BookId &&
+			Book.Equals(product.Book);
+			//Comments?.Count == product.Comments?.Count;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Id);
+		}
+
+		public AutoIncProduct ToAutoInc()
+		{
+			return new AutoIncProduct()
+			{
+				Id = Id,
+				BookId = BookId,
+				Book = Book,
+				Comments = Comments
+			};
+		}
+	}
+}
