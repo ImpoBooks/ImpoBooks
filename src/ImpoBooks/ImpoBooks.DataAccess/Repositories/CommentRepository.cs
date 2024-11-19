0a64460050804522e5964ee519416e228a3aeda1
Nazg0r <gorchinshiy83@gmail.com>
2024-11-19 17:49:40 +0200

    feat: implement CommentRepository

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Repositories/CommentRepository.cs
@@ -0,0 +1,30 @@
+﻿using ImpoBooks.DataAccess.Entities.AutoIncremented;
+using ImpoBooks.DataAccess.Entities;
+using ImpoBooks.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase;
+using Supabase.Postgrest.Responses;
+using static Supabase.Postgrest.Constants;
+
+namespace ImpoBooks.DataAccess.Repositories
+{
+	public class CommentRepository(Client client) : Repository<Comment, AutoIncComment>(client), ICommentRepository
+	{
+		private readonly Client _client = client;
+
+		public async Task<Comment> GetByUserName(string name)
+		{
+			ModeledResponse<Comment> response = await _client.From<Comment>()
+				.Select("*, Users!inner(*))")
+				.Filter("Users.name", Operator.Equals, name)
+				.Get();
+
+			Comment comment = response.Model;
+			return comment;
+		}
+	}
+}
