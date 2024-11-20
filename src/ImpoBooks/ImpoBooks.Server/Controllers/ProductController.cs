5fa71f775ac4465d0bf642eab766d9a24f767615
Nazg0r <gorchinshiy83@gmail.com>
2024-11-20 19:52:08 +0200

    feat: add GetProduct controller

---
+++ src/ImpoBooks/ImpoBooks.Server/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using ErrorOr;
+using ImpoBooks.BusinessLogic.Services.Models;
+using ImpoBooks.BusinessLogic.Services.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImpoBooks.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController(IProductService productService) : Controller
+	{
+		private readonly IProductService _productService = productService;
+
+		[HttpGet("{id}")]
+		[ProducesResponseType<ProductModel>(StatusCodes.Status200OK)]
+		[ProducesResponseType<List<Error>>(StatusCodes.Status404NotFound)]
+		public async Task<IResult> GetProduct(int id)
+		{
+			ErrorOr<ProductModel> result = await _productService.GetProductAsync(id);
+
+			return result.Match(
+				books => Results.Ok(books),
+				errors => Results.BadRequest(errors.First())
+			);
+		}
+	}
+}
