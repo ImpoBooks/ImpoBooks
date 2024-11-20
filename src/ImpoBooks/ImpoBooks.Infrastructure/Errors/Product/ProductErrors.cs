cd079429eda43f18da4e10c60d3b1c3bbe1c74a3
Nazg0r <gorchinshiy83@gmail.com>
2024-11-20 19:46:00 +0200

    feat: add error type of Product service

---
+++ src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Product/ProductErrors.cs
@@ -0,0 +1,19 @@
+﻿using ErrorOr;
+using ImpoBooks.BusinessLogic.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.Infrastructure.Errors.Product
+{
+	public static class ProductErrors
+	{
+		public static Error ProductIdIsZero =>
+			Error.Custom(ErrorTypes.WrongInfo, "Product.Id.WrongInfo", "Product id was equal to zero.");
+
+		public static Error ProductNotFound =>
+			Error.Custom(ErrorTypes.NotFound, "Product.NotFound", "Failed to get product.");
+	}
+}
