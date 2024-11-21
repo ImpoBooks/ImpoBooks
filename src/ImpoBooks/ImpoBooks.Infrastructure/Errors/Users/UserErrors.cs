d04ad4d4428dea2693f7b472c31b2fafaf31f80f
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-21 21:56:41 +0200

    fix: UserErrors code for already existig user

--- src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Users/UserErrors.cs
+++ src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Users/UserErrors.cs
@@ -6,7 +6,7 @@ namespace ImpoBooks.BusinessLogic.Errors.Users;
 public static class UserErrors
 {
     public static Error AlreadyExists =>
-        Error.Custom(ErrorTypes.AlreadyExists, "User.Exists", "User with this email already exists.");
+        Error.Custom(ErrorTypes.AlreadyExists, "User.AlreadyExists", "User with this email already exists.");
 
     public static Error IsNull =>
         Error.Custom(ErrorTypes.IsNull, "User.IsNull", "User was null.");
