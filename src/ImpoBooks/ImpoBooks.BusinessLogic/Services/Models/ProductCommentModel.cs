d28a09029c5333a700b55a55c618b854d219c9f7
Nazg0r <gorchinshiy83@gmail.com>
2024-11-21 11:50:19 +0200

    feat: add ProductCommentModel

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Models/ProductCommentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Models
+{
+	public class ProductCommentModel
+	{
+		public int UserId { get; set; }
+		public string Content { get; set; }
+		public decimal Rating { get; set; }
+	}
+}
