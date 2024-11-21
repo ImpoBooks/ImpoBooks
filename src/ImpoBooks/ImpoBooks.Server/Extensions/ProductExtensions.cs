8395299a7830ba3e63f122ec628241264b486772
Nazg0r <gorchinshiy83@gmail.com>
2024-11-21 13:47:05 +0200

    feat: add CommentCreateRequest class

---
+++ src/ImpoBooks/ImpoBooks.Server/Extensions/ProductExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ImpoBooks.BusinessLogic.Services.Models;
+using ImpoBooks.Server.Requests;
+
+namespace ImpoBooks.Server.Extensions
+{
+	public static class ProductExtensions
+	{
+		public static ProductCommentModel ToModel(this CommentCreateRequest source)
+		{
+			return new ProductCommentModel()
+			{
+				UserId = source.UserId,
+				Content = source.Content,
+				Rating = source.Rating
+			};
+		}
+	}
+}
