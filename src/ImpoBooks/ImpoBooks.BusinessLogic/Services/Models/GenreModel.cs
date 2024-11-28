af55203a363d3076f87c73aa6e8b1b2785e9f1d4
Nazg0r <gorchinshiy83@gmail.com>
2024-11-28 23:09:52 +0200

    feat: add genre model

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Models/GenreModel.cs
@@ -0,0 +1,20 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Models
+{
+	public class GenreModel
+	{
+		public string Name { get; set; }
+		public override bool Equals(object obj)
+		{
+			return obj is GenreModel book &&
+			Name == book.Name;
+
+		}
+	}
+}
