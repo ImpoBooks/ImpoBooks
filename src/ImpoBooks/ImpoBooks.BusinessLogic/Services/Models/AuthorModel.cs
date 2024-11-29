d78fb54e7fb4b125642a9893d97a806c331798b2
Nazg0r <gorchinshiy83@gmail.com>
2024-11-29 13:13:32 +0200

    feat: add author model

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Models/AuthorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Models
+{
+	public class AuthorModel
+	{
+		public string Name { get; set; }
+
+		public override bool Equals(object? obj)
+		{
+			return obj is AuthorModel author &&
+			Name == author.Name;
+		}
+	}
+}
