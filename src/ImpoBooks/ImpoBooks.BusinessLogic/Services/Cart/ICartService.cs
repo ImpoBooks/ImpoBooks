025db0e84bbe1de45aee411a3a687272c2a3e2f9
Nazg0r <gorchinshiy83@gmail.com>
2024-12-03 19:37:06 +0200

    feat: implement CartService class

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Cart/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using ErrorOr;
+using ImpoBooks.BusinessLogic.Services.Models;
+using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Cart
+{
+	public interface ICartService
+	{
+		Task<ErrorOr<Success>> CheckAvailabilityAsync(IEnumerable<OrderProductModel> products);
+		Task<ErrorOr<Success>> SendToPaymentServiceAsync(CustomerRequisitesModel requisites);
+		Task<ErrorOr<Order>> SaveOrderAsync(OrderModel orderModel);
+	}
+}
