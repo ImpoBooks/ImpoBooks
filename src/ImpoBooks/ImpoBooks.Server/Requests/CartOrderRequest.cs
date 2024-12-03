de3479b68e112216efe0fa9148961383fda479a1
Nazg0r <gorchinshiy83@gmail.com>
2024-12-03 22:06:33 +0200

    feat: add CartOrderRequest

---
+++ src/ImpoBooks/ImpoBooks.Server/Requests/CartOrderRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImpoBooks.Server.Requests
+{
+	public class CartOrderRequest
+	{
+		[Required] public string FirstName { get; set; }
+		[Required] public string LastName { get; set; }
+		[Required] public string Email { get; set; }
+		[Required] public string Address { get; set; }
+		[Required] public string City { get; set; }
+		[Required] public string ZipCode { get; set; }
+		[Required] public string Country { get; set; }
+		[Required] public string CardNumber { get; set; }
+		[Required] public string ExpirationDate { get; set; }
+		[Required] public string CVV { get; set; }
+		[Required] public decimal TotalSum { get; set; }
+
+		[Required] public IEnumerable<ProductInfo> Products { get; set; }
+	}
+
+	public record ProductInfo(int Id, int Count);
+}
