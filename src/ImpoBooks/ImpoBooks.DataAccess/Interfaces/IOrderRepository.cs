4d2266aee1f6532d1c0280fa7a780dff817f58b7
Nazg0r <gorchinshiy83@gmail.com>
2024-12-03 12:33:59 +0200

    feat: implement OrderRepository

---
+++ src/ImpoBooks/ImpoBooks.DataAccess/Interfaces/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.DataAccess.Interfaces
+{
+	public interface IOrderRepository : IRepository<Order>
+	{
+		Task<Order> GetByOrderCodeAsync(int code);
+
+	}
+}
