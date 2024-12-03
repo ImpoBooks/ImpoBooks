2f677d5ee628d367fb6839cdc5db3be14c1ce2ab
Nazg0r <gorchinshiy83@gmail.com>
2024-12-03 19:35:54 +0200

    feat: add error types of Cart service

---
+++ src/ImpoBooks/ImpoBooks.Infrastructure/Errors/Cart/CartErrors.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+using ImpoBooks.BusinessLogic.Errors;
+
+namespace ImpoBooks.Infrastructure.Errors.Cart
+{
+	public static class CartErrors
+	{
+		public static Error OrderProductsIsNull =>
+			Error.Custom(ErrorTypes.IsNull, "OrderProducts.IsNull", "Order product models was null.");
+		public static Error RequisitesIsNull =>
+			Error.Custom(ErrorTypes.IsNull, "Requisites.IsNull", "Customer requisites model was null.");
+		public static Error OrderModelIsNull =>
+			Error.Custom(ErrorTypes.IsNull, "OrderModel.IsNull", "Order model object was null.");
+		public static Error ProductNotFound =>
+			Error.Custom(ErrorTypes.NotFound, "Product.NotFound", "Failed to find order product in DB.");
+		public static Error OrderNotFound =>
+			Error.Custom(ErrorTypes.NotFound, "Order.NotFound", "Failed to get order.");
+		public static Error OrderCodeAlreadyExists =>
+			Error.Custom(ErrorTypes.AlreadyExists, "OrderCode.AlreadyExists", "Order with such code already exists.");
+	}
+}
