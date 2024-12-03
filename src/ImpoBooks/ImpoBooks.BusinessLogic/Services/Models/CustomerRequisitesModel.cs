236611ffdd94e227135033bc490b0dc88b78a6bb
Nazg0r <gorchinshiy83@gmail.com>
2024-12-03 12:40:29 +0200

    feat: add order-related models

---
+++ src/ImpoBooks/ImpoBooks.BusinessLogic/Services/Models/CustomerRequisitesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.BusinessLogic.Services.Models
+{
+	public class CustomerRequisitesModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string CardNumber { get; set; }
+		public string ExpirationDate { get; set; }
+		public string CVV { get; set; }
+		public decimal TotalSum { get; set; }
+	}
+}
