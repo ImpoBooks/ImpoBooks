ff9aa833ccb8e0515f96b321a90bcff8131111c4
Nazg0r <gorchinshiy83@gmail.com>
2024-12-05 00:03:30 +0200

    feat: improve CommentRepositoryTests

--- src/ImpoBooks/ImpoBooksTests/Integration/DataTests/CommentRepositoryTests.cs
+++ src/ImpoBooks/ImpoBooksTests/Integration/DataTests/CommentRepositoryTests.cs
@@ -1,57 +1,25 @@
 using ImpoBooks.DataAccess.Entities;
 using ImpoBooks.DataAccess.Repositories;
 using ImpoBooks.Tests.Integration.Fixtures;
-using Microsoft.Extensions.Configuration;
+using ImpoBooks.Tests.Integration.Seeds;
 using Supabase;
-using System.Linq;
-using Xunit;
-using Xunit.Abstractions;
-using Xunit.Sdk;
+using Supabase.Postgrest.Responses;
 
 namespace ImpoBooks.Tests.Integration.DataTests
 {
 	[Collection("Integration Tests Collection")]
 	public class CommentRepositoryTests : IClassFixture<CommentSupabaseFIxture>
 	{
-		private readonly CommentSupabaseFIxture _fixture;
-		private readonly Client _client;
 		private readonly CommentRepository _repository;
 		private IEnumerable<Comment> _preparedComments;
-		private IEnumerable<User> _preparedUsers;
-		private IEnumerable<Author> _preparedAuthors;
-		private IEnumerable<Publisher> _preparedPublishers;
-		private IEnumerable<Book> _preparedBooks;
+		private readonly Client _client;
+
 
 		public CommentRepositoryTests(CommentSupabaseFIxture fixture)
 		{
-			_fixture = fixture;
 			_client = fixture.client;
 			_repository = new(fixture.client);
-			_preparedAuthors = fixture.PreparedAuthors
-				.Select(x => new Author()
-				{
-					Id = x.Id,
-					PersonId = x.PersonId,
-					Person = fixture.PreparedPersons.First(p => p.Id == x.PersonId)
-				});
-			_preparedPublishers = fixture.PreparedPublishers;
-			_preparedBooks = fixture.PreparedBooks
-				.Select(x => new Book()
-				{
-					Id = x.Id,
-					PublisherId = x.PublisherId,
-					AuthorId = x.AuthorId,
-					Name = x.Name,
-					Description = x.Description,
-					ReleaseDate = x.ReleaseDate,
-					Price = x.Price,
-					Rating = x.Rating,
-					Format = x.Format,
-					Publisher = _preparedPublishers.First(p => p.Id == x.PublisherId),
-					Author = _preparedAuthors.First(a => a.Id == x.AuthorId)
-				});
-			_preparedUsers = fixture.PreparedUsers;
-			_preparedComments = fixture.PreparedComments.Select(c => new Comment
+			_preparedComments = CommentSeeder.PreparedComments.Select(c => new Comment
 			{
 				Id = c.Id,
 				UserId = c.UserId,
@@ -60,7 +28,7 @@ namespace ImpoBooks.Tests.Integration.DataTests
 				LikesNumber = c.LikesNumber,
 				DislikesNumber = c.DislikesNumber,
 				Rating = c.Rating,
-				User = _preparedUsers.FirstOrDefault(u => u.Id == c.UserId)!
+				User = UserSeeder.PreparedUsers.FirstOrDefault(u => u.Id == c.UserId)!
 			});
 		}
 
@@ -72,13 +40,14 @@ namespace ImpoBooks.Tests.Integration.DataTests
 		[InlineData(5)]
 		public async Task GetByIdAsync_ReturnExpectedComment(int Id)
 		{
-			Thread.Sleep(500);
-
 			//Arrange
 			Comment expected = _preparedComments.FirstOrDefault(x => x.Id == Id)!;
 
 			//Act
 			Comment comment = await _repository.GetByIdAsync(expected.Id);
+
+			ModeledResponse<Comment> response = await _client.From<Comment>().Get();
+			IEnumerable<Comment> entity = response.Models;
 
 			//Assert
 			Assert.Equal(expected, comment);
@@ -91,8 +60,6 @@ namespace ImpoBooks.Tests.Integration.DataTests
 		[InlineData("Nikita")]
 		public async Task GetByUserName_ReturnExpectedComment(string name)
 		{
-			Thread.Sleep(500);
-
 			//Arrange
 			IEnumerable<Comment> expected = _preparedComments.Where(x => x.User.Name == name)!;
 
@@ -106,8 +73,6 @@ namespace ImpoBooks.Tests.Integration.DataTests
 		[Fact]
 		public async Task GetAllAsync_ReturnExpectedCommentsAmount()
 		{
-			Thread.Sleep(500);
-
 			//Arrange
 			int expected = _preparedComments.Count();
 
@@ -121,8 +86,6 @@ namespace ImpoBooks.Tests.Integration.DataTests
 		[Fact]
 		public async Task GetAllAsync_ReturnExpectedCommentsContent()
 		{
-			Thread.Sleep(500);
-
 			//Arrange
 			IEnumerable<Comment> expected = _preparedComments;
 
@@ -145,14 +108,23 @@ namespace ImpoBooks.Tests.Integration.DataTests
 			//Act
 			await _repository.CreateAsync(expected);
 			Comment actualComment = await _repository.GetByIdAsync(caseId);
-			expected.User = _preparedUsers.FirstOrDefault(u => u.Id == expected.UserId)!;
+			expected.User = UserSeeder.PreparedUsers.FirstOrDefault(u => u.Id == expected.UserId)!;
 
 			//Assert
 			Assert.Equal(expected, actualComment);
 
-			await IntegrationTestHelper.RecreateTable(_client, _preparedUsers);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedProducts);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedComments);
+			await IntegrationTestHelper.RefreshDb
+			(
+				UserSeeder.Seed +
+				PersonSeeder.Seed +
+				AuthorSeeder.Seed +
+				PublisherSeeder.Seed +
+				GenreSeeder.Seed +
+				BookSeeder.Seed +
+				BookGenreSeeder.Seed +
+				ProductSeeder.Seed +
+				CommentSeeder.Seed
+			);
 		}
 
 		[Theory]
@@ -167,14 +139,23 @@ namespace ImpoBooks.Tests.Integration.DataTests
 			//Act
 			await _repository.UpdateAsync(expected);
 			Comment actualComment = await _repository.GetByIdAsync(caseId);
-			expected.User = _preparedUsers.FirstOrDefault(u => u.Id == expected.UserId)!;
+			expected.User = UserSeeder.PreparedUsers.FirstOrDefault(u => u.Id == expected.UserId)!;
 
 			//Assert
 			Assert.Equal(expected, actualComment);
 
-			await IntegrationTestHelper.RecreateTable(_client, _preparedUsers);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedProducts);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedComments);
+			await IntegrationTestHelper.RefreshDb
+			(
+				UserSeeder.Seed +
+				PersonSeeder.Seed +
+				AuthorSeeder.Seed +
+				PublisherSeeder.Seed +
+				GenreSeeder.Seed +
+				BookSeeder.Seed +
+				BookGenreSeeder.Seed +
+				ProductSeeder.Seed +
+				CommentSeeder.Seed 
+			);
 		}
 
 		[Theory]
@@ -193,9 +174,18 @@ namespace ImpoBooks.Tests.Integration.DataTests
 			//Assert
 			Assert.Null(actualComment);
 
-			await IntegrationTestHelper.RecreateTable(_client, _preparedUsers);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedProducts);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedComments);
+			await IntegrationTestHelper.RefreshDb
+			(
+				UserSeeder.Seed +
+				PersonSeeder.Seed +
+				AuthorSeeder.Seed +
+				PublisherSeeder.Seed +
+				GenreSeeder.Seed +
+				BookSeeder.Seed +
+				BookGenreSeeder.Seed +
+				ProductSeeder.Seed +
+				CommentSeeder.Seed 
+			);
 		}
 
 		[Theory]
@@ -213,9 +203,18 @@ namespace ImpoBooks.Tests.Integration.DataTests
 			//Assert
 			Assert.Null(actualComment);
 
-			await IntegrationTestHelper.RecreateTable(_client, _preparedUsers);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedProducts);
-			await IntegrationTestHelper.RecreateTable(_client, _fixture.PreparedComments);
+			await IntegrationTestHelper.RefreshDb
+			(
+				UserSeeder.Seed +
+				PersonSeeder.Seed +
+				AuthorSeeder.Seed +
+				PublisherSeeder.Seed +
+				GenreSeeder.Seed +
+				BookSeeder.Seed +
+				BookGenreSeeder.Seed +
+				ProductSeeder.Seed +
+				CommentSeeder.Seed 
+			);
 		}
 
 		private IEnumerable<Comment> NewComments =>
