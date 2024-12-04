cdcecda0e9314b56d43b86a8bdf71fb3a79ea1aa
Nazg0r <gorchinshiy83@gmail.com>
2024-12-04 21:38:54 +0200

    feat: add PublisherSeeder

---
+++ src/ImpoBooks/ImpoBooksTests/Integration/Seeds/PublisherSeeder.cs
@@ -0,0 +1,27 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.Tests.Integration.Seeds
+{
+	public static class PublisherSeeder
+	{
+		public static IEnumerable<Publisher> PreparedPublishers =>
+			new Publisher[]
+			{
+				new() { Id = 1, Name = "Ranok"},
+				new() { Id = 2, Name = "Smoloskyp"},
+				new() { Id = 3, Name = "Old Lion Publishing House"},
+				new() { Id = 4, Name = "Nash Format"},
+				new() { Id = 5, Name = "Vivat"}
+			};
+
+		public static string Seed =
+			"INSERT INTO \"Publishers\" (id, name) VALUES" +
+			string.Join(", ", PreparedPublishers.Select(p => $"({p.Id}, '{p.Name}')")) +
+			";";
+	}
+}
