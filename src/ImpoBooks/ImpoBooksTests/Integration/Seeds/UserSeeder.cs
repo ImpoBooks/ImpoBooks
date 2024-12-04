32785b2fa615bc72224eeae91b31055669f86a57
Nazg0r <gorchinshiy83@gmail.com>
2024-12-05 00:02:18 +0200

    upd: add user role to UserSeeder data

--- src/ImpoBooks/ImpoBooksTests/Integration/Seeds/UserSeeder.cs
+++ src/ImpoBooks/ImpoBooksTests/Integration/Seeds/UserSeeder.cs
@@ -12,15 +12,15 @@ namespace ImpoBooks.Tests.Integration.Seeds
 		public static IEnumerable<User> PreparedUsers =>
 			new User[]
 			{
-				new() {Id = 1, Name = "Mikhail"},
-				new() {Id = 2, Name = "Bender"},
-				new() {Id = 3, Name = "Kyle"},
-				new() {Id = 4, Name = "Nikita"},
+				new() {Id = 1, Name = "Mikhail", RoleId = 2},
+				new() {Id = 2, Name = "Bender", RoleId = 2},
+				new() {Id = 3, Name = "Kyle", RoleId = 2},
+				new() {Id = 4, Name = "Nikita", RoleId = 2},
 			};
 
 		public static string Seed =
-			"INSERT INTO \"Users\" (id, name) VALUES" +
-			string.Join(", ", PreparedUsers.Select(g => $"({g.Id}, '{g.Name}')")) +
+			"INSERT INTO \"Users\" (id, name, role_id) VALUES" +
+			string.Join(", ", PreparedUsers.Select(u => $"({u.Id}, '{u.Name}', {u.RoleId})")) +
 			";";
 	}
 }
