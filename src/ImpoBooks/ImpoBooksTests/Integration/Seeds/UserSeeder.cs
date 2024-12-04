92baef1619c8c6bd811c9a25f1ce86c38b14f206
Nazg0r <gorchinshiy83@gmail.com>
2024-12-04 21:42:42 +0200

    feat: add UserSeeder

---
+++ src/ImpoBooks/ImpoBooksTests/Integration/Seeds/UserSeeder.cs
@@ -0,0 +1,26 @@
+﻿using ImpoBooks.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpoBooks.Tests.Integration.Seeds
+{
+	public static class UserSeeder
+	{
+		public static IEnumerable<User> PreparedUsers =>
+			new User[]
+			{
+				new() {Id = 1, Name = "Mikhail"},
+				new() {Id = 2, Name = "Bender"},
+				new() {Id = 3, Name = "Kyle"},
+				new() {Id = 4, Name = "Nikita"},
+			};
+
+		public static string Seed =
+			"INSERT INTO \"Users\" (id, name) VALUES" +
+			string.Join(", ", PreparedUsers.Select(g => $"({g.Id}, '{g.Name}')")) +
+			";";
+	}
+}
